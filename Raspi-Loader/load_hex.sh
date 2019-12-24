bea8713a2a777ee166723189f4cf531ba2ccca41
Elliott Partridge <elliott.partridge@gmail.com>
2019-12-24 04:07:47 +0000

    Implemented hex load script

--- Raspi-Loader/load_hex.sh
+++ Raspi-Loader/load_hex.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+hexfile="$1"
+
+sudo gpio -g mode 22 out
+sudo gpio -g write 22 0
+sudo /usr/local/bin/avrdude -V -p t85 -P /dev/spidev0.0 -c linuxspi -b 10000 -U flash:w:$hexfile
+sudo gpio -g write 22 1
